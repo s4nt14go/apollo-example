7b170b2754a24c815e5e01ed4f87f2faaed97f8e
s4nt14go <santiagopergamo@gmail.com>
2021-10-20 08:44:13 -0300

    feat(appsync): put item in table for mutation 'addTodo'

--- full-stack/todo-list/todo-list-appsync/functions/addTodo.ts
+++ full-stack/todo-list/todo-list-appsync/functions/addTodo.ts
@@ -1,11 +1,23 @@
 import { AppSyncResolverHandler } from "aws-lambda";
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+import { ulid } from 'ulid'
+
+const DocumentClient = new DynamoDB.DocumentClient();
+const { TODOS_TABLE } = process.env;
 
 export const handler: AppSyncResolverHandler<any, any> = async (event) => {
     console.log('event', event);
     const { description } = event.arguments;
 
-    return {
-        id: 'dummy-id',
+    const Item = {
+        id: ulid(),
         description,
-    };
+    }
+
+    await DocumentClient.put({
+        TableName: TODOS_TABLE,
+        Item,
+    }).promise();
+
+    return Item;
 }
