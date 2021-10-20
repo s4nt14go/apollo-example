b7e1bda96e7946818cf3e7567ff68a3978fd31ce
s4nt14go <santiagopergamo@gmail.com>
2021-10-20 08:45:15 -0300

    feat(appsync): get item in table for query 'todo'

--- full-stack/todo-list/todo-list-appsync/functions/todo.ts
+++ full-stack/todo-list/todo-list-appsync/functions/todo.ts
@@ -1,10 +1,17 @@
 import { AppSyncResolverHandler } from "aws-lambda";
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+
+const DocumentClient = new DynamoDB.DocumentClient();
+const { TODOS_TABLE } = process.env;
 
 export const handler: AppSyncResolverHandler<any, any> = async (event) => {
     console.log('event', event);
+    const { id } = event.arguments;
 
-    return {
-        id: 'dummy-id',
-        description: 'dummy-description',
-    };
+    const { Item: todo } = await DocumentClient.get({
+        TableName: TODOS_TABLE,
+        Key: { id },
+    }).promise();
+
+    return todo;
 }
