a613f6fb59bf030a98497cef681388607a6ff94a
s4nt14go <santiagopergamo@gmail.com>
2021-10-20 08:33:21 -0300

    feat(appsync): add lambda resolver for query 'todo'
    Returns dummy data for now

---
+++ full-stack/todo-list/todo-list-appsync/functions/todo.ts
@@ -0,0 +1,10 @@
+import { AppSyncResolverHandler } from "aws-lambda";
+
+export const handler: AppSyncResolverHandler<any, any> = async (event) => {
+    console.log('event', event);
+
+    return {
+        id: 'dummy-id',
+        description: 'dummy-description',
+    };
+}
